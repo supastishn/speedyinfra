4afd4d8faaef9362ecf81740f691f5bbd3d93324
supastishn <omareeto2012@hotmail.com>
2025-07-26 16:38:12 +0000

    feat: load Eruda on all pages and add spacing below navbar

--- examples/react/src/main.jsx
+++ examples/react/src/main.jsx
@@ -5,10 +5,8 @@ import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './AuthContext'
 
-// Initialize Eruda console in development
-if (import.meta.env.MODE === 'development') {
-  import('eruda').then(eruda => eruda.default.init());
-}
+// Initialize Eruda console unconditionally
+import('eruda').then(eruda => eruda.default.init());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
